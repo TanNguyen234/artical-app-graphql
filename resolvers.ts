3c24ce6f8cc19961a016ea6db14e838ad48f22ad
TanNguyen234 <tanntd.2005@gmail.com>
2024-10-17 17:09:15 +0700

    Lay ra chi tiet 1 bai viet

--- resolvers.ts
+++ resolvers.ts
@@ -13,5 +13,16 @@ export const resolvers = {
    ;
       return articles;
     },
+
+    getArticle: async (_ : any, args: any) => {
+        const { id } = args;
+        
+        const article = await Artical.findOne({
+          _id: id,
+          deleted: false,
+        });
+     ;
+        return article;
+    },
   },
 };
