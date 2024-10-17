bce48a0d45e54a915b02131f7759f3a17f0a0530
TanNguyen234 <tanntd.2005@gmail.com>
2024-10-17 16:58:32 +0700

    Lay ra danh sach bai viet

--- resolvers.ts
+++ resolvers.ts
@@ -1,7 +1,17 @@
+import Artical from "./models/artical.model";
+
 export const resolvers = {
-    Query: {
-      hello: () => {
-        return "Hello World!";
-      },
+  Query: {
+    hello: () => {
+      return "Hello World!";
     },
+
+    getListArticle: async () => {
+      const articles = await Artical.find({
+        deleted: false,
+      });
+   ;
+      return articles;
+    },
+  },
 };
